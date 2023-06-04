e22857095634ae3b5c7a6eaa9c0abea15724e50d
Yaryna Bilan <yabi@rib-software.dk>
2023-06-04 17:02:48 +0300

    first task

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+        int year = 2023;
+    public static int sumOfTwoNumbers (int a, int b) {
+        int c = a + b; //sum of two numbers
+        return c;
+    }
+    public static void main(String[]args){
+        sumOfTwoNumbers(2,3);
+    }
+}
