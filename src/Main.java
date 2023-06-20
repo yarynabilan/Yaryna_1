2f26aeded886e5c4e86255338571312004b9a447
Yaryna Bilan <yabi@rib-software.dk>
2023-06-20 16:39:46 +0300

    resolving of conflict

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
+    public static int sumOfTwoNumbers(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
     public static void main(String[] args) {
         int monthNumber = 6;
 
-        H3Month h3Month = new H3Month(); // Створення екземпляра класу H3Month
-        int numberOfDays = h3Month.getNumberOfDays(monthNumber); // Виклик методу через екземпляр класу
+        H3Month h3Month = new H3Month(); // Creating an instance of the H3Month class
+        int numberOfDays = h3Month.getNumberOfDays(monthNumber); // Calling a method using the instance
+
+        int year = 2023;
+        int result = sumOfTwoNumbers(2, 3);
+        System.out.println("Sum of two numbers is " + result);
 
     }
 }
+
