99e0c12ed6336e748352464eae3b537ffd2b073a
Yaryna Bilan <yabi@rib-software.dk>
2023-06-20 16:09:20 +0300

    resolve conflict in Main file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
     public static void main(String[] args) {
         int monthNumber = 6;
@@ -6,6 +5,5 @@ public class Main {
         H3Month h3Month = new H3Month(); // Створення екземпляра класу H3Month
         int numberOfDays = h3Month.getNumberOfDays(monthNumber); // Виклик методу через екземпляр класу
 
-        System.out.println("Number of days: " + numberOfDays);
     }
 }
